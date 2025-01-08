426e4477ec2db0ae06657dca42bb12bf661aedad
GiornoGiovanaJoJo <57575835+GiornoGiovanaJoJo@users.noreply.github.com>
2025-01-08 15:35:08 +0300

    fix

--- eco_track_project/urls.py
+++ eco_track_project/urls.py
@@ -6,6 +6,10 @@ from django.conf.urls.static import static
 from drf_yasg.views import get_schema_view
 from drf_yasg import openapi
 from rest_framework_simplejwt import views as jwt_views  # <-- Импорт jwt_views
+from rest_framework_simplejwt.views import (
+    TokenObtainPairView,
+    TokenRefreshView,
+)
 
 schema_view = get_schema_view(
     openapi.Info(
@@ -22,7 +26,11 @@ schema_view = get_schema_view(
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('eco_api/', include('eco_api.urls')),  # <-- Оставили только одну строку
+    path('eco_api/', include('eco_api.urls')),
+    path('api/', include('eco_api.urls')),
+    path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
+    # <-- Оставили только одну строку
     path('token/', jwt_views.TokenObtainPairView.as_view(), name='token_obtain_pair'),  # <-- Добавили URL для TokenObtainPairView
     re_path(r'^swagger(?P<format>\.json|\.yaml)$', schema_view.without_ui(cache_timeout=0), name='schema-json'),
     re_path(r'^swagger/$', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
